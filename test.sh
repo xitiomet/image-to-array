cce05cb6ef86eaecd18a0959affd3c68d0733d6d
xitiomet <brian@openstatic.org>
2021-09-03 18:01:12 -0400

    Improved JNI supprot, and fixed missing references for graalvm

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn clean package
-java -jar target/image-to-array-1.0-SNAPSHOT.jar $*
+java -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image/ -jar target/image-to-array-1.0-SNAPSHOT.jar -i hope.jpg -s 64x64 -d -a -h
+java -agentlib:native-image-agent=config-merge-dir=src/main/resources/META-INF/native-image/ -jar target/image-to-array-1.0-SNAPSHOT.jar -i https://openstatic.org/ -b > /dev/null
